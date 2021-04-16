019f041cf45dbaefd938bea767d7f2be9e25813f
Otunba Marcus <otunbamarcusademola@gmail.com>
2021-04-16 12:03:48 +0100

    changed

---
+++ push.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+http
+  .get('http://jsonplaceholder.typicode.com/posts', res => {
+    let data = ''
+
+    res.on('data', chunk => {
+      data += chunk
+    })
+
+    res.on('end', () => {
+      fs.mkdir(path.join(__dirname, '/result'), (err) => {
+        if (err) throw err
+      })
+      fs.writeFile(path.join(__dirname, '/result', 'posts.json'), data, (err) => {
+        if (err) throw err
+        console.log('Folder writen to ...');
+      })
+    })
+  })
+  .on('error', err => {
+    console.log('Error:' + err.message);
+  })
